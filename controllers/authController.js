dca671a1102e63a35c2fa97057fcab5e076db08f
mirnaihab <mirnaehab14@gmail.com>
2024-08-18 00:49:28 +0300

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const generateToken = require('../utils/generateToken');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+
+};
+
+exports.signin = async (req, res) => {
+
+};
+
+exports.forgotPassword = async (req, res) => {
+
+};
